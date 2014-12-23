719696565b67b42ec539a3521360a93860adbc3d
deanar <rdeanar@gmail.com>
2014-12-23 11:53:30 +0300

    Removed columns comments from migration

--- migrations/m140516_113603_create_file_storage_table.php
+++ migrations/m140516_113603_create_file_storage_table.php
@@ -8,17 +8,17 @@ class m140516_113603_create_file_storage_table extends \yii\db\Migration
     {
         $this->createTable('fp_uploads', [
             'id' => 'pk',
-            'timestamp' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT "Время загрузки"',
-            'type' => Schema::TYPE_STRING . ' DEFAULT NULL COMMENT "Тип"',
-            'type_id' => Schema::TYPE_INTEGER . ' DEFAULT NULL COMMENT "ID Типа"',
-            'hash' => Schema::TYPE_STRING . ' DEFAULT NULL COMMENT "HASH"',
-            'ord' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT "0" COMMENT "Порядок отображения"',
-            'filename' => Schema::TYPE_STRING . ' NOT NULL COMMENT "Имя файла"',
-            'original' => Schema::TYPE_STRING . ' NOT NULL COMMENT "Оригинальное имя файла"',
-            'mime' => Schema::TYPE_STRING . ' NOT NULL DEFAULT "" COMMENT "Тип файла"',
-            'size' => Schema::TYPE_INTEGER . ' NOT NULL COMMENT "Размер файла"',
-            'width' => Schema::TYPE_INTEGER . ' DEFAULT NULL COMMENT "Ширина"',
-            'height' => Schema::TYPE_INTEGER . ' DEFAULT NULL COMMENT "Высота"',
+            'timestamp' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
+            'type' => Schema::TYPE_STRING . ' DEFAULT NULL',
+            'type_id' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
+            'hash' => Schema::TYPE_STRING . ' DEFAULT NULL',
+            'ord' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT "0"',
+            'filename' => Schema::TYPE_STRING . ' NOT NULL',
+            'original' => Schema::TYPE_STRING . ' NOT NULL',
+            'mime' => Schema::TYPE_STRING . ' NOT NULL DEFAULT ""',
+            'size' => Schema::TYPE_INTEGER . ' NOT NULL COMMENT',
+            'width' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
+            'height' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
         ]);
     }
 
