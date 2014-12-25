bf26b25122871ccfeb775dcdc84392d0c5791fb3
deanar <rdeanar@gmail.com>
2014-12-25 22:35:59 +0300

    fixed migration, added table Index

--- migrations/m140516_113603_create_file_storage_table.php
+++ migrations/m140516_113603_create_file_storage_table.php
@@ -6,7 +6,7 @@ class m140516_113603_create_file_storage_table extends \yii\db\Migration
 {
     public function up()
     {
-        $this->createTable('fp_uploads', [
+        $this->createTable('{{%fp_uploads}}', [
             'id' => 'pk',
             'timestamp' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
             'type' => Schema::TYPE_STRING . ' DEFAULT NULL',
@@ -16,10 +16,13 @@ class m140516_113603_create_file_storage_table extends \yii\db\Migration
             'filename' => Schema::TYPE_STRING . ' NOT NULL',
             'original' => Schema::TYPE_STRING . ' NOT NULL',
             'mime' => Schema::TYPE_STRING . ' NOT NULL DEFAULT ""',
-            'size' => Schema::TYPE_INTEGER . ' NOT NULL COMMENT',
+            'size' => Schema::TYPE_INTEGER . ' NOT NULL',
             'width' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
             'height' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
         ]);
+
+        $this->createIndex('type_type_id', '{{%fp_uploads}}', ['type', 'type_id']);
+        $this->createIndex('type_hash', '{{%fp_uploads}}', ['type', 'hash']);
     }
 
     public function down()
